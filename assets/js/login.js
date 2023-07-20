37baf4354654ef0895186d25bf28c1e78c819c4b
Felipe-Belotto <felipeeduardo817@gmail.com>
2023-07-20 03:50:35 -0300

    Adicionando tela de login, tela de administrador e funcionalidades

---
+++ assets/js/login.js
@@ -0,0 +1,15 @@
+const botaologar = document.getElementById("botaoLogar");
+const inputEmail = document.getElementById("inputEmail");
+const inputSenha = document.getElementById("inputSenha");
+
+botaologar.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  if (inputEmail.value == "admin@email.com" && inputSenha.value == "admin") {
+    window.location.href = "/assets/html/telaADMIN.html";
+  } else {
+    alert(
+      " Para entrar na sessão de administrador \n Email: admin@email.com \n Senha: admin"
+    );
+  }
+});
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,9 @@
 import { exibeCard } from "./criaCard.js";
 
+let botaoLogin = document.getElementById("btnLogin");
+
+botaoLogin.addEventListener("click", () => {
+  window.location.href = "/assets/html/login.html";
+});
+
 exibeCard();
