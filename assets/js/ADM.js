a720a9ba1d390eba6ec66230417848b209824724
Felipe-Belotto <felipeeduardo817@gmail.com>
2023-07-21 03:39:32 -0300

    solução definitiva para o bug da lista de produtos

--- assets/js/ADM.js
+++ assets/js/ADM.js
@@ -5,26 +5,24 @@ async function exibeCardADM() {
   const dadosAPI = await recebeAPI();
   const listaTodosProdutos = document.getElementById("listaTodosProdutos");
 
-  dadosAPI.forEach((element, index) => {
+  dadosAPI.forEach((element) => {
     const card = criarCard(element.imagem, element.name, element.preco);
-
-    // atribui um id para cada card criado
-    card.setAttribute("data-id", index);
-
     listaTodosProdutos.appendChild(card);
+
     const botaoDeletar = card.querySelector(".botao__deletar");
+
     botaoDeletar.addEventListener("click", () => {
-      // Reconhece qual o id do botão que foi clicado
-      const cardIndex = botaoDeletar.parentElement.getAttribute("data-id");
-      card.remove();
-      // corta o ID que o botão foi clicado
-      dadosAPI.splice(cardIndex, 1);
-      // Atualiza a lista de produtos no localStorage
-      localStorage.setItem("todosProdutos", JSON.stringify(dadosAPI));
+      const cardIndex = dadosAPI.indexOf(element);
+      if (cardIndex !== -1) {
+        dadosAPI.splice(cardIndex, 1);
+
+        localStorage.setItem("todosProdutos", JSON.stringify(dadosAPI));
+
+        card.remove();
+      }
     });
   });
 
-  // Salva a lista de todos os produtos no localStorage
   localStorage.setItem("todosProdutos", JSON.stringify(dadosAPI));
 }
 
