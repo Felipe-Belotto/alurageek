9a0f185f8e13eba80de7236f66d24323e4cd5723
Felipe-Belotto <felipeeduardo817@gmail.com>
2023-07-21 03:15:02 -0300

    Alterando funcionamento de sincronização da lista dos produtos

--- assets/js/exibeCard.js
+++ assets/js/exibeCard.js
@@ -2,29 +2,49 @@ import { recebeAPI } from "./recebeAPI.js";
 import { criarCard } from "./criaCard.js";
 
 async function exibeCard() {
-  const listaTodosProdutos = document.getElementById("listaTodosProdutos");
+  const dadosAPI = await recebeAPI();
+
   const listaStarWars = document.getElementById("listaStarWars");
   const listaConsoles = document.getElementById("listaConsoles");
   const listaDiversos = document.getElementById("listaDiversos");
 
-  // Obtém a lista de todos os produtos do localStorage
-  const todosProdutos = JSON.parse(localStorage.getItem("todosProdutos"));
+  if (localStorage.getItem("todosProdutos") == null) {
+    dadosAPI.forEach((element) => {
+      const card = criarCard(element.imagem, element.name, element.preco);
 
-  todosProdutos.forEach((element) => {
-    const card = criarCard(element.imagem, element.name, element.preco);
+      switch (element.categoria) {
+        case "Star Wars":
+          listaStarWars.appendChild(card);
+          break;
+        case "Consoles":
+          listaConsoles.appendChild(card);
+          break;
+        case "Diversos":
+          listaDiversos.appendChild(card);
+          break;
+      }
+    });
+  }
 
-    switch (element.categoria) {
-      case "Star Wars":
-        listaStarWars.appendChild(card);
-        break;
-      case "Consoles":
-        listaConsoles.appendChild(card);
-        break;
-      case "Diversos":
-        listaDiversos.appendChild(card);
-        break;
-    }
-  });
+  if (localStorage.getItem("todosProdutos") !== null) {
+    const todosProdutos = JSON.parse(localStorage.getItem("todosProdutos"));
+
+    todosProdutos.forEach((element) => {
+      const card = criarCard(element.imagem, element.name, element.preco);
+
+      switch (element.categoria) {
+        case "Star Wars":
+          listaStarWars.appendChild(card);
+          break;
+        case "Consoles":
+          listaConsoles.appendChild(card);
+          break;
+        case "Diversos":
+          listaDiversos.appendChild(card);
+          break;
+      }
+    });
+  }
 }
 
 export { exibeCard };
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,3 @@
-import { exibeCardADM } from "./ADM.js";
 import { exibeCard } from "./exibeCard.js";
 
 let botaoLogin = document.getElementById("btnLogin");
@@ -7,5 +6,4 @@ botaoLogin.addEventListener("click", () => {
   window.location.href = "/assets/html/login.html";
 });
 
-exibeCardADM();
 exibeCard();
