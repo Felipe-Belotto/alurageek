03eff28960f94a4c5c1422d0ea5600b633a8d9f3
Felipe Belotto <felipeeduardo817@gmail.com>
2023-07-27 16:05:29 -0300

    melhorando usabilidade dos inputs da tela de adicionar produto

---
+++ assets/js/addProduto.js
@@ -0,0 +1,27 @@
+/* As divs que recebe os inputs e label */
+const divUrl = document.getElementById("addUrl");
+const divCategoria = document.getElementById("addCategoria");
+const divNome = document.getElementById("addNome");
+const divPreco = document.getElementById("addPreco");
+
+/* Inputs */
+const inputUrl = document.getElementById("produtoUrl");
+const inputCategoria = document.getElementById("produtoCategoria");
+const inputNome = document.getElementById("produtoNome");
+const inputPreco = document.getElementById("produtoPreco");
+
+divUrl.addEventListener("click", () => {
+  inputUrl.focus();
+});
+
+divCategoria.addEventListener("click", () => {
+  inputCategoria.focus();
+});
+
+divNome.addEventListener("click", () => {
+  inputNome.focus();
+});
+
+divPreco.addEventListener("click", () => {
+  inputPreco.focus();
+});
