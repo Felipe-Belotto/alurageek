8dd3c269f6eccc4c1edd499cf4c61d62b860861f
Felipe-Belotto <felipeeduardo817@gmail.com>
2023-07-27 22:22:35 -0300

    Adicionando botão na tela add produto

--- assets/js/addProduto.js
+++ assets/js/addProduto.js
@@ -1,3 +1,9 @@
+const botaoTelaADM = document.getElementById("btnTelaADM");
+
+botaoTelaADM.addEventListener("click", () => {
+  window.location.href = "./admin.html";
+});
+
 /* As divs que recebe os inputs e label */
 const divUrl = document.getElementById("addUrl");
 const divCategoria = document.getElementById("addCategoria");
