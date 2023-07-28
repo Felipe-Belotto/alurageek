05d354980ef151eede2a76e1d3520ee4f883111c
Felipe Belotto <felipeeduardo817@gmail.com>
2023-07-28 13:04:34 -0300

    Revert "Adicionando função de alterar produtos ( fase de teste )"
    This reverts commit cd940a558f9108bd294c0cbf8c8a5596e13675ea.

--- assets/js/ADM.js
+++ assets/js/ADM.js
@@ -1,10 +1,7 @@
 import { recebeAPI } from "./recebeAPI.js";
 import { criarCard } from "./criaCard.js";
-import {
-  enviarNovoProduto,
-  deletarProduto,
-  alteraProduto,
-} from "./poderesAdm.js";
+
+const botaoAlteraProduto = document.getElementById("botaoAlteraProduto");
 
 async function exibeCardADM() {
   const dadosAPI = await recebeAPI();
@@ -35,22 +32,14 @@ async function exibeCardADM() {
       }
     });
 
-    const botaoAlteraProduto = document.getElementById("botaoAlteraProduto");
-
-    botaoAlteraProduto.addEventListener("click", () => {
-      const cardSelecionado = card.id;
-      console.log(cardSelecionado);
-      alteraProduto(cardSelecionado);
-    });
-
-    const botaoSectionAddProduto = document.getElementById(
-      "botaoSectionAddProduto"
-    );
+    const botaoAlterar = card.querySelector(".botao__alterar");
     const TelaListaTodosProdutos = document.getElementById("produtosEditaveis");
     const TelaAddProduto = document.getElementById("addProduto");
     const botaoTelaADM = document.getElementById("btnTelaADM");
+    const botaoSectionAddProduto = document.getElementById(
+      "botaoSectionAddProduto"
+    );
     const botaoAddProduto = document.getElementById("botaoAddProduto");
-    const botaoAlterar = card.querySelector(".botao__alterar");
 
     TelaAddProduto.style.display = "none";
 
@@ -79,3 +68,26 @@ async function exibeCardADM() {
 }
 
 exibeCardADM();
+alternaTela();
+
+export { exibeCardADM };
+
+async function deletarProduto(id) {
+  const response = await fetch(
+    `https://64b8785621b9aa6eb079e1c0.mockapi.io/produtos/${id}`,
+    {
+      method: "DELETE",
+    }
+  );
+
+  if (response.status === 200) {
+    console.log(
+      "O item " + localStorage.getItem(`card${id}`) + " foi apagado com SUCESSO"
+    );
+    window.location.reload();
+  } else {
+    console.log(
+      "Não foi possivel apagar o item " + localStorage.getItem(`card${id}`)
+    );
+  }
+}
--- assets/js/addProduto.js
+++ assets/js/addProduto.js
@@ -27,26 +27,6 @@ divPreco.addEventListener("click", () => {
   inputPreco.focus();
 });
 
-async function deletarProduto(id) {
-  const response = await fetch(
-    `https://64b8785621b9aa6eb079e1c0.mockapi.io/produtos/${id}`,
-    {
-      method: "DELETE",
-    }
-  );
-
-  if (response.status === 200) {
-    console.log(
-      "O item " + localStorage.getItem(`card${id}`) + " foi apagado com SUCESSO"
-    );
-    window.location.reload();
-  } else {
-    console.log(
-      "Não foi possivel apagar o item " + localStorage.getItem(`card${id}`)
-    );
-  }
-}
-
 async function enviarNovoProduto() {
   fetch("https://64b8785621b9aa6eb079e1c0.mockapi.io/produtos", {
     method: "POST",
@@ -65,34 +45,6 @@ async function enviarNovoProduto() {
     .then((json) => console.log(json));
 }
 
-async function mostraProduto(id) {
-  const response = await fetch(
-    `https://64b8785621b9aa6eb079e1c0.mockapi.io/produtos/${id}`
-  );
-  const cadastro = await response.json();
+const botaoSubmit = document.getElementById("botaoAddProduto");
 
-  inputNome.value = cadastro.name;
-  inputUrl.value = cadastro.imagem;
-  inputPreco.value = cadastro.preco;
-  inputDescricao.value = cadastro.descricao;
-  inputCategoria.value = cadastro.categoria;
-}
-
-async function alteraProduto(id) {
-  mostraProduto();
-  fetch(`https://64b8785621b9aa6eb079e1c0.mockapi.io/produtos/${id}`, {
-    method: "PUT",
-    body: JSON.stringify({
-      name: inputNome.value,
-      imagem: inputUrl.value,
-      preco: inputPreco.value,
-      descricao: inputDescricao.value,
-      categoria: inputCategoria.value,
-    }),
-    headers: {
-      "Content-type": "application/json; charset=UTF-8",
-    },
-  });
-}
-
-export { deletarProduto, enviarNovoProduto, alteraProduto };
+botaoSubmit.addEventListener("click", enviarNovoProduto);
