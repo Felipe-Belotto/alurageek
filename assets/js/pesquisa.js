fdb013c1fc685d89ee8d4d20783efbac5885aedb
Felipe-Belotto <felipeeduardo817@gmail.com>
2023-08-02 22:42:52 -0300

    começando a implementar função de pesquisa

--- assets/js/pesquisa.js
+++ assets/js/pesquisa.js
@@ -3,19 +3,38 @@ import { recebeAPI } from "./recebeAPI.js";
 async function pesquisa() {
   const dadosApi = await recebeAPI();
   const inputPesquisa = document.getElementById("inputPesquisa");
+  const listaProdutosPesquisa = document.getElementById(
+    "listaProdutosPesquisa"
+  );
 
-  const produtosParecidos = [];
+  const resultadoContainer = document.getElementById("resultadoPesquisa");
 
-  inputPesquisa.addEventListener("change", () => {
-    if (inputPesquisa.value !== "") {
+  function removeAcentos(str) {
+    return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+  }
+
+  inputPesquisa.addEventListener("input", () => {
+    const produtosParecidos = [];
+    const termoPesquisado = removeAcentos(
+      inputPesquisa.value.trim().toLowerCase()
+    );
+
+    if (termoPesquisado !== "") {
       dadosApi.forEach((element) => {
-        const elementValue = element.name.toLowerCase();
+        const elementValue = removeAcentos(element.name.toLowerCase());
 
-        if (elementValue.includes(inputPesquisa.value)) {
+        if (elementValue.includes(termoPesquisado)) {
           produtosParecidos.push(element);
+
+          resultadoContainer.style.display = "block";
+
+          listaProdutosPesquisa.innerHTML = element.name;
         }
+
+        console.log(termoPesquisado);
       });
-      console.log(produtosParecidos);
+    } else {
+      resultadoContainer.style.display = "none";
     }
   });
 }
